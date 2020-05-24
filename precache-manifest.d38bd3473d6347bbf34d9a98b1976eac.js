4fcfe3f5097bceec48985178fdf3512b9f331c4a
iamsukanta <sukantabalacste2016@gmail.com>
2020-05-24 23:50:38 +0600

    Updates

---
+++ precache-manifest.d38bd3473d6347bbf34d9a98b1976eac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49851d088473e74147590f0c33079444",
+    "url": "/reactjs-deploy/index.html"
+  },
+  {
+    "revision": "ec778b0ac9489a6b18b1",
+    "url": "/reactjs-deploy/static/css/main.372eaf11.chunk.css"
+  },
+  {
+    "revision": "c5ac4540ff8d1d621ade",
+    "url": "/reactjs-deploy/static/js/2.b62d61e0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/reactjs-deploy/static/js/2.b62d61e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec778b0ac9489a6b18b1",
+    "url": "/reactjs-deploy/static/js/main.13460340.chunk.js"
+  },
+  {
+    "revision": "861c1a9c1dff83cbebeb",
+    "url": "/reactjs-deploy/static/js/runtime-main.1d2d6231.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactjs-deploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
